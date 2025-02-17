8c1432ec3cec62dedd64848d9c61ce893bd0cda2
Akrao99 <ab2687@njit.edu>
2025-02-17 13:27:56 -0500

    Updated local code

--- RecipeappTests/DiskImageCache.swift
+++ RecipeappTests/DiskImageCache.swift
@@ -1,35 +1,38 @@
-//
-//  DiskImageCache.swift
-//  RecipeappTests
-//
-//  Created by Ak B on 2/17/25.
-//
-
 import XCTest
+@testable import Recipeapp
 
-final class DiskImageCache: XCTestCase {
+final class DiskImageCacheTests: XCTestCase {
+
+    var diskCache: DiskImageCache!
+    let testURL = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/small.jpg")!
+    let testData = "Fake data".data(using: .utf8)!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        diskCache = DiskImageCache()
+        
+        if !FileManager.default.fileExists(atPath: diskCache.cacheDirectory.path) {
+            try FileManager.default.createDirectory(at: diskCache.cacheDirectory,
+                                                      withIntermediateDirectories: true,
+                                                      attributes: nil)
         }
     }
 
+    override func tearDownWithError() throws {
+        // delete cache
+        let fileURL = diskCache.fileURL(for: testURL)
+        try? FileManager.default.removeItem(at: fileURL)
+        diskCache = nil
+    }
+
+    func testCacheAndRetrieveImageData() throws {
+        // Cache data
+        diskCache.cacheImageData(testData, for: testURL)
+        
+        // Retrieve cachedata.
+        let retrievedData = diskCache.cachedImageData(for: testURL)
+        
+        // Checking
+        XCTAssertNotNil(retrievedData, "Retrieved data should not be nil.")
+        XCTAssertEqual(testData, retrievedData, "The cached data should match the original test data.")
+    }
 }
--- RecipeappTests/RecipeappTests.swift
+++ RecipeappTests/RecipeappTests.swift
@@ -1,21 +1,21 @@
+////
+////  RecipeappTests.swift
+////  RecipeappTests
+////
+////  Created by Ak B on 2/14/25.
+////
 //
-//  RecipeappTests.swift
-//  RecipeappTests
+//import Testing
+//import XCTest
+//@testable import Recipeapp
 //
-//  Created by Ak B on 2/14/25.
+//struct RecipeappTests {
+//    final class RecipeappTests: XCTestCase {
+//         
+//    }
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//        
+//    }
 //
-
-import Testing
-import XCTest
-@testable import Recipeapp
-
-struct RecipeappTests {
-    final class RecipeappTests: XCTestCase {
-         
-    }
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        
-    }
-
-}
+//}
